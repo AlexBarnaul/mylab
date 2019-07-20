64424d2a72c883a1d0a46c3f88c83bf7fbcea818
AlexBarnaul <bigmishko@mail.ru>
2019-07-20 11:36:39 +0300

    packagejscon cahnges

--- gruntfile.js
+++ gruntfile.js
@@ -33,8 +33,8 @@ module.exports = function (grunt) {
 				}
 			}
 		},
-		/*
-				//POSTCSS
+		
+/*				//POSTCSS
 				postcss: {
 					options: {
 						processors: [
@@ -53,25 +53,6 @@ module.exports = function (grunt) {
 					}
 
 				},*/
-
-		//UNCSS
-		uncss: {
-			dist: {
-				files: {
-					"source/css/style.css": ["source/*.html"]
-				}
-			}
-		},
-
-		//SVGSTORE
-		svgstore: {
-			options: {},
-			default: {
-				files: {
-					'source/img/sprite-photo.svg': ['source/img/spritePhoto/*.svg'],
-				},
-			},
-		},
 
 		//COPY
 		copy: {
@@ -92,72 +73,12 @@ module.exports = function (grunt) {
 				force: true
 			},
 			build: ["../build/"]
-		},
-
-		//POSTHTML
-		posthtml: {
-			options: {
-				use: [require('posthtml-include')({
-					encoding: 'utf-8'
-				})]
-			},
-			build: {
-				files: [{
-					expand: true,
-					src: ['*.html'],
-					dest: '../build/'
-		        }]
-			}
-		},
-
-
-		//CWEBP 
-		cwebp: {
-			/*			 static: {
-			 	files: {
-			 		'dist/img-png.webp': 'src/img.png',
-			 		'dist/img-jpg.webp': 'src/img.jpg',
-			 		'dist/img-gif.webp': 'src/img.gif'
-			 	}
-			 },*/
-			dynamic: {
-				options: {
-					q: 85
-				},
-				files: [{
-					expand: true,
-					cwd: 'source/img/',
-					src: ['source/**/*.{png,jpg}'],
-					dest: 'source/img/webp'
-        }]
-			}
-		},
-
-		//IMAGEMIN
-		imagemin: {
-			images: {
-				options: {
-					optimizationLevel: 7,
-					progressive: true
-				},
-				files: [{
-					expand: true,
-					src: ["source/img/*.{jpg,png}"],
-					dest: "."
- }]
-			}
 		}
 
-
 	});
-	grunt.loadNpmTasks('grunt-juwain-posthtml');
 	grunt.loadNpmTasks('grunt-csso');
-	grunt.loadNpmTasks('grunt-cwebp');
-	grunt.loadNpmTasks('grunt-contrib-imagemin');
-	grunt.loadNpmTasks('grunt-svgstore');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-uncss');
 	grunt.loadNpmTasks('grunt-postcss');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
